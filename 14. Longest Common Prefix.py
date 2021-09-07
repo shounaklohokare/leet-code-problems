9bf527842919b6980d4ecd5c3505f855343573bf
Shounak Lohokare <41364885+shounaklohokare@users.noreply.github.com>
2021-09-07 18:23:18 +0530

    Create 14. Longest Common Prefix.py

---
+++ 14. Longest Common Prefix.py
@@ -0,0 +1,18 @@
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        
+        longest_pre = ""
+        
+        if not strs: return longest_pre
+        
+        shortest_str = min(strs, key=len)
+        
+        for i in range(len(shortest_str)):
+            
+            if all([x.startswith(shortest_str[:i+1]) for x in strs]):
+                longest_pre = shortest_str[:i+1]
+                
+        return longest_pre
+        
+        
+        
