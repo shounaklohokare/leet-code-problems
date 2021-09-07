8ce3769ebe566f542bfd6174d8c31eda5b249526
Shounak Lohokare <41364885+shounaklohokare@users.noreply.github.com>
2021-09-07 18:40:20 +0530

    Create 27. Remove Element.py

---
+++ 27. Remove Element.py
@@ -0,0 +1,10 @@
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        
+        i = -1
+        for j in range(len(nums)):
+            if nums[j]!=val:
+                i+=1
+                nums[i]=nums[j]
+                
+        return i+1
