a707c01065e1655363689c7bb7e6a8e2aad1f9ce
Shounak Lohokare <41364885+shounaklohokare@users.noreply.github.com>
2021-10-07 21:30:54 +0530

    Update 14. Longest Common Prefix.py

--- 14. Longest Common Prefix.py
+++ 14. Longest Common Prefix.py
@@ -1,18 +1,12 @@
-class Solution:
-    def longestCommonPrefix(self, strs: List[str]) -> str:
-        
-        longest_pre = ""
-        
-        if not strs: return longest_pre
-        
-        shortest_str = min(strs, key=len)
-        
-        for i in range(len(shortest_str)):
-            
-            if all([x.startswith(shortest_str[:i+1]) for x in strs]):
-                longest_pre = shortest_str[:i+1]
-                
-        return longest_pre
-        
-        
-        
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        if (strs.length == 0) return "";
+        String prefix = strs[0];
+        for (int i = 1; i < strs.length; i++)
+            while (strs[i].indexOf(prefix) != 0) {
+                prefix = prefix.substring(0, prefix.length() - 1);
+                if (prefix.isEmpty()) return "";
+            }        
+    return prefix;
+}
+}
