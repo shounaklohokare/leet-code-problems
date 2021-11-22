c12a5f251b5e6769fc58afdc4a3e5e4be1bc0465
Shounak Lohokare <41364885+shounaklohokare@users.noreply.github.com>
2021-11-22 17:29:54 +0530

    Update 1854. Maximum Population Year.java

--- 1854. Maximum Population Year.java
+++ 1854. Maximum Population Year.java
@@ -4,24 +4,26 @@ class Solution {
         int[] years = new int[2051];
         
         for(int[] log : logs){
-            years[log[0]]+=1;
-            years[log[1]]-=1;
+            years[log[0]]+=1; // if born +1
+            years[log[1]]-=1; // if die -1
         }
         
-        int cnt=0;
-        int max = Integer.MIN_VALUE;
+        int cnt=0;            // num of people alive at instance
+        int result=0;         // earliest year with max population
+        int max = Integer.MIN_VALUE; // max population
         
-        int result=-1;
-        
-        for(int i=1950; i<2051; i++){    
+        for(int i=1950; i<2051; i++){
+            
             cnt+=years[i];
             
-            if(cnt > max){
+            if(cnt>max){  
                 max=cnt;
                 result=i;
             }
+    
         }
         
         return result;
+    
     }
 }
