25baa72fc1a0f467b7ba2291a325d35d80e71062
Shounak Lohokare <41364885+shounaklohokare@users.noreply.github.com>
2021-11-30 12:28:00 +0530

    Create 53. Maximum Subarray.java

---
+++ 53. Maximum Subarray.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        
+        int sum=nums[0];
+        int max=nums[0];
+        
+        for(int i=1; i<nums.length; i++){
+            sum=Math.max(nums[i], nums[i]+sum);
+            max=Math.max(sum, max);
+        }
+        
+        
+        return max;
+        
+        
+    }
+}
