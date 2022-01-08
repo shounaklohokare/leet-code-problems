8e81ea1a2f308f9babb88ea3fcdb7ca6ab1afef9
Shounak Lohokare <41364885+shounaklohokare@users.noreply.github.com>
2022-01-08 22:59:30 +0530

    Update 1346. Check If N and Its Double Exist.sql

--- 1346. Check If N and Its Double Exist.sql
+++ 1346. Check If N and Its Double Exist.sql
@@ -1,41 +1,37 @@
 class Solution {
     public boolean checkIfExist(int[] arr) {
-        int n = arr.length;
+        
         Arrays.sort(arr);
+        
+        int n = arr.length;
         for(int i=0; i<n; i++){
             
-
-            
             int target = arr[i] * 2;
+            
             int x = binarySearch(arr, target);
-            if(x!=-1 && x!=i){
-                return true;
-            }
+            
+            if(x!=i && x!=-1)  return true;
+            
         }
         
         return false;
+        
     }
     
-    int binarySearch(int arr[], int target){
+    int binarySearch(int[] arr, int target){
         
         int lo = 0;
-        int hi = arr.length-1;
+        int hi = arr.length - 1;
         
-        while(lo<=hi){
+        while(lo <= hi){
             
-            int mid = lo + (hi - lo ) / 2;
+            int mid = lo + (hi-lo) / 2;
             
-            if(arr[mid]==target){
-                return mid;
-            }
+            if(arr[mid]==target) return mid;
             
-            else if(arr[mid] < target){
-                lo = mid + 1;
-            }
+            if(arr[mid] < target) lo = mid+1;
             
-            else {
-                hi = mid - 1;
-            }
+            else hi = mid - 1;
             
         }
         
