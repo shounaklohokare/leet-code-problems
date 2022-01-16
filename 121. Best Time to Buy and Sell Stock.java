21a73b9405bb2638968f8bbc04fdae633cefa3aa
Shounak Lohokare <41364885+shounaklohokare@users.noreply.github.com>
2022-01-16 16:27:25 +0530

    Create 121. Best Time to Buy and Sell Stock.java

---
+++ 121. Best Time to Buy and Sell Stock.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        
+        int maxSoFar = 0;
+        int maxCur = 0;
+        for(int i=1; i<prices.length; i++){
+            maxCur = Math.max(0, maxCur + prices[i] - prices[i-1]);
+            maxSoFar = Math.max(maxCur, maxSoFar);
+        }
+        
+        return maxSoFar;
+        
+    }
+}
