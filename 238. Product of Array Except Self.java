3ff37d0f51af5bde2d5f3d306c62577ab6296311
Shounak Lohokare <41364885+shounaklohokare@users.noreply.github.com>
2022-02-03 21:16:14 +0530

    Update 238. Product of Array Except Self.java

--- 238. Product of Array Except Self.java
+++ 238. Product of Array Except Self.java
@@ -1,21 +1,29 @@
-public class Solution {
+class Solution {
     public int[] productExceptSelf(int[] nums) {
+        
         int n = nums.length;
+        
         int[] res = new int[n];
-        // Calculate lefts and store in res.
+        
         int left = 1;
-        for (int i = 0; i < n; i++) {
-            if (i > 0)
-                left = left * nums[i - 1];
-            res[i] = left;
+        for(int i=0; i<n; i++){
+            
+            if(i > 0) left *= nums[i-1];     // calculate product of numbers which are on every numbers left 
+            
+            res[i] = left;          // assign that product in o/p array
+            
         }
-        // Calculate rights and the product from the end of the array.
-        int right = 1;
-        for (int i = n - 1; i >= 0; i--) {
-            if (i < n - 1)
-                right = right * nums[i + 1];
-            res[i] *= right;
+        
+        int right=1;
+        for(int i=n-1; i>=0; i--){
+            
+            if(i < n-1) right *= nums[i+1];     // calculate product of numbers which are on every numbers right
+            
+            res[i] *= right;          // multiply it with product of numbers on every numbers left
+            
         }
+        // product of every number in the array except itself is the product of number on its left multiplied by product of evey number on its right
         return res;
+        
     }
 }
