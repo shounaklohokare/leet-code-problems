346be7f632872ad9a5dc7d880f2e011ad84bd0ba
Shounak Lohokare <41364885+shounaklohokare@users.noreply.github.com>
2022-02-05 23:29:41 +0530

    Update 34. Find First and Last Position of Element in Sorted Array.java

--- 34. Find First and Last Position of Element in Sorted Array.java
+++ 34. Find First and Last Position of Element in Sorted Array.java
@@ -1,11 +1,15 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
+     
+        // in an array sorted in an ascending order
+        // the first occurence of a number is on the left hand side of the array
+        // and last occurence of a number is on the right hand side of the array
         
-        int[] out = {-1, -1};
+        int[] out = new int[] {-1, -1};
         
-        out[0] = binarySearch(nums, target, true);
+        out[0] = binarySearch(nums, target, true);  // as in the first element of o/p array we have to send the starting position of target in array, we send true as a parameter
         
-        if(out[0]>-1) out[1] = binarySearch(nums, target, false);
+        if(out[0] > -1) out[1] = binarySearch(nums, target, false); // as in the second element of the o/p array we have to send the ending position of target in array, thus we send false as parameter
         
         return out;
         
@@ -20,25 +24,25 @@ class Solution {
         
         while(lo <= hi){
             
-            int mid = lo + (hi-lo) / 2;
+            int mid = lo + (hi - lo) / 2;
             
             if(nums[mid]==target){
                 ans = mid;
                 
-                if(isFirstOccurence) hi = mid-1;
+                if(isFirstOccurence) hi = mid-1; // if target is at mid and it is the first occurence then we try to look at another target element at left of array thus we assign hi to mid-1
                 
-                else lo = mid+1;
+                else lo = mid+1; // if target is at mid and it not the first occurence that is the last occurence then we assign lo to mid+1
+                
             }
-            else if(nums[mid]<target){
-                lo = mid+1;
-            }
-            else{
-                hi = mid-1;
-            }
+            
+            else if(nums[mid] < target) lo = mid+1;
+            
+            else hi = mid-1;
             
         }
         
         return ans;
+        
         
     }
 }
