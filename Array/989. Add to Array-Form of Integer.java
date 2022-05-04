ed58632f2a882b1398cfccb946f680fbccf4639b
Shounak Lohokare <shounaklohokare@gmail.com>
2022-05-04 22:41:08 +0530

    added explanation to 989

--- Array/989. Add to Array-Form of Integer.java
+++ Array/989. Add to Array-Form of Integer.java
@@ -1,21 +1,27 @@
 class Solution {
     public List<Integer> addToArrayForm(int[] num, int k) {
-        
-        List<Integer> al = new ArrayList<>();
-        
+
+        List<Integer> out = new ArrayList<>();
+
         int i = num.length;
-        int curr = k;
-        while(--i >=0 || curr > 0){
-            if(i>=0){
-                curr+=num[i];
+        int curr = k; // number to be added in the array form
+        while (--i >= 0 || curr > 0) {
+            // run loop while
+            // i is greater than or equal to zero
+            // and while curr is greater than zero
+
+            if (i >= 0) {
+                // add num element at i
+                // in curr
+                curr += num[i];
             }
-            
-            al.add(curr%10);
-            curr/=10;
+
+            out.add(0, curr % 10); // add modulus 10 of curr in arraylist
+            curr /= 10; // floor divide curr by 10
+
         }
-        
-        Collections.reverse(al);
-        return al;
-        
+
+        return out;
+
     }
 }
