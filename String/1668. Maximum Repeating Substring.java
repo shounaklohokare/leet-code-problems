95c11c35d183b5ac158663d728ac0605a81b1b4d
Shounak Lohokare <shounaklohokare@gmail.com>
2022-06-22 18:07:34 +0530

    added Next Greater Element I

--- String/1668. Maximum Repeating Substring.java
+++ String/1668. Maximum Repeating Substring.java
@@ -2,11 +2,17 @@ class Solution {
     public int maxRepeating(String sequence, String word) {
 
         int out = 1;
-        // repeats word incrementally and checks whether it exists in string
+        // .contains() return boolean value
+        // repeat(int count) repeats the word by value of count
         while (sequence.contains(word.repeat(out))) {
+            // we iteratively increase value of out
+            // which means we iteratively check whether
+            // the word repeating out times contains in string
+            // if it does we increment out and check
             out++;
         }
 
-        return out - 1;
+        return out - 1; // when it's false we return it by substracting 1
+
     }
 }
