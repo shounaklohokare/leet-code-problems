b607f66f9a9ff8e99dab056ab3dde94447356a3a
Shounak Lohokare <shounaklohokare@gmail.com>
2023-01-03 23:32:00 +0530

    added 15. 3sum

--- Array/1. Two Sum.java
+++ Array/1. Two Sum.java
@@ -1,13 +1,19 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        
-        for(int i=0; i<nums.length; i++){
-            for(int j=i+1; j<nums.length; j++){
-                if(nums[i]+nums[j]==target) return new int[] {i, j};
+
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+
+        for (int i = 0; i < nums.length; i++) {
+
+            int x = nums[i], y = target - x;
+
+            if (map.containsKey(y)) {
+                return new int[] { map.get(y), i };
             }
+
+            map.put(x, i);
+
         }
-        
-        return null;
-        
+        return new int[] {};
     }
 }
